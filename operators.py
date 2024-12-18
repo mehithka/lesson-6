2a1ea61345bbea6e051d464e3aed18d3e412ca99
mehithka <mehith.karacasia@gmail.com>
2024-12-18 15:21:49 +0500

    operators a1

---
+++ operators.py
@@ -0,0 +1,29 @@
+a = 10
+b = 9
+c = 12
+
+if a and b and c :
+    print("all the values of the boleans are true")
+else:
+      print("the values of the booleans are not true")
+
+a = 10
+b = -9
+c = 22
+
+if a>0 or b>-10:
+     print("the condition is true")
+
+else:
+     print("the condition is false")
+
+if b>0 or c>543:
+     print("the value is true")
+
+else:
+     print("the value is flase")
+
+if a>0 or c>=10 :
+     print("the condition is tru")
+else:
+     print("the condition is false")
