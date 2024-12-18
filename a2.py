ab067a049f02fe3c4b76b72e09533aece92bba50
mehithka <mehith.karacasia@gmail.com>
2024-12-18 15:35:25 +0500

    a2

---
+++ a2.py
@@ -0,0 +1,28 @@
+a = 2
+b = 9
+c = 9
+
+print(a != b)
+print(b != c)
+
+d = "coding"
+e = "python"
+
+if d != e :
+ print(d,"and",e,"are not same ")
+else:
+ print("they both are the same ")
+
+w = 41
+x = 12
+
+if (w == 21) != (x == 12):
+ print("over all the statement is true",)
+
+f = int(input("enter a number"))
+
+if f%2 != 0 :
+ print("the remainder of the division of your number with two is not an even number")
+
+else:
+ print("the remainder of the division of your number with two is an even number")
